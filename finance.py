c58eac9eebb5a00b8714748a9eea33539b420651
naman_yadav <namanyadav@gmail.com>
2019-06-04 19:54:26 -0400

    read from csv

--- finance.py
+++ finance.py
@@ -6,8 +6,12 @@ import pandas_datareader.data as web
 
 style.use('ggplot');
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
+# start = dt.datetime(2000, 1, 1)
+# end = dt.datetime(2016, 12, 31)
+#
+# df = web.DataReader('TSLA', 'yahoo', start, end)
+# df.to_csv('tesla.csv')
 
-df = web.DataReader('TSLA', 'yahoo', start, end)
-print(df.tail())
+# to read csv
+df = pd.read_csv('tesla.csv', parse_dates=True, index_col=0)
+print(df.head())
