3dfaa381251f93560bf8331b7ae527e558c87540
naman_yadav <namanyadav@gmail.com>
2019-06-04 20:17:05 -0400

    create sub plots from data frame

--- finance.py
+++ finance.py
@@ -11,7 +11,15 @@ end = dt.datetime(2016, 12, 31)
 
 df = web.DataReader('TSLA', 'yahoo', start, end)
 
-df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
 # df.dropna(inplace=True)
+print(df.head())
 
-print(df.tail())
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
