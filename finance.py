da196e4d35bf97084d7e2e90c80d3d129e09eacc
naman_yadav <namanyadav@gmail.com>
2019-06-04 20:24:07 -0400

    open high low close based on every 10 days of data

--- finance.py
+++ finance.py
@@ -10,16 +10,14 @@ start = dt.datetime(2000, 1, 1)
 end = dt.datetime(2016, 12, 31)
 
 df = web.DataReader('TSLA', 'yahoo', start, end)
+# df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
 
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-# df.dropna(inplace=True)
-print(df.head())
+df_ohlc = df['Adj Close'].resample('10D').ohlc()  # open high low close
+df_volume = df['Volume'].resample('10D').sum()
+# open high low close based on every 10 days of data
+print(df_ohlc.head())
 
 ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
 
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
-
-plt.show()
+# plt.show()
