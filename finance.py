e6ff99af1acca0aa6da4aa9275201458c87a793f
naman_yadav <namanyadav@gmail.com>
2019-06-04 20:39:24 -0400

    ohlc with candlestick

--- finance.py
+++ finance.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -14,10 +16,18 @@ df = web.DataReader('TSLA', 'yahoo', start, end)
 
 df_ohlc = df['Adj Close'].resample('10D').ohlc()  # open high low close
 df_volume = df['Volume'].resample('10D').sum()
+df_ohlc.reset_index(inplace=True)
+
+# converting date to datetime
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 # open high low close based on every 10 days of data
-print(df_ohlc.head())
+# print(df_ohlc.head())
 
 ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
 
-# plt.show()
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
+plt.show()
