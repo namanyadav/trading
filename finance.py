f71884a63f8c456951c18e42884853c0b07921f8
naman_yadav <namanyadav@gmail.com>
2019-06-04 20:04:53 -0400

    add new column to data frame

--- finance.py
+++ finance.py
@@ -6,14 +6,12 @@ import pandas_datareader.data as web
 
 style.use('ggplot');
 
-# start = dt.datetime(2000, 1, 1)
-# end = dt.datetime(2016, 12, 31)
-#
-# df = web.DataReader('TSLA', 'yahoo', start, end)
-# df.to_csv('tesla.csv')
+start = dt.datetime(2000, 1, 1)
+end = dt.datetime(2016, 12, 31)
 
-# to read csv
-df = pd.read_csv('tesla.csv', parse_dates=True, index_col=0)
-# print(df.head())
-df['Adj Close'].plot()
-plt.show()
+df = web.DataReader('TSLA', 'yahoo', start, end)
+
+df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+# df.dropna(inplace=True)
+
+print(df.tail())
