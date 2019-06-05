456e99939f6f1cc834ae102dc95da0bfece97743
naman_yadav <namanyadav@gmail.com>
2019-06-04 21:58:03 -0400

    get data for each ticker from yahoo finance and save into csv files

--- snp500.py
+++ snp500.py
@@ -1,4 +1,8 @@
 import bs4 as bs  # beautiful soup
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle  # serializes any python object
 import requests
 
@@ -19,4 +23,35 @@ def save_sp500_tickers():
     return tickers
 
 
-save_sp500_tickers()
+# save_sp500_tickers()
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500tickers.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2016, 12, 31)
+
+    for ticker in tickers:
+        ticker = ticker[:-1]  # Scraping from wikipedia returns 'MMM/n' to the pickle file.
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            if ticker != 'BBT' and ticker != 'BRK.B' and ticker != 'BF.B' and ticker != 'CF' and ticker != 'CTVA'\
+                    and ticker != 'COTY' and ticker != 'DOW' and ticker != 'DD' and ticker != 'EW'\
+                    and ticker != 'EVRG' and ticker != 'HIG' and ticker != 'LIN' and ticker != 'OKE'\
+                    and ticker != 'PNW' and ticker != 'PPG' and ticker != 'DGX' and ticker != 'VMC'\
+                    and ticker != 'ZBH':
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+    print('completed')
+
+get_data_from_yahoo()
